a7a32c3fa13249631bcc4edb7bb0a1e0a6964beb
codewithgupta <118753775+codewithgupta@users.noreply.github.com>
2023-05-10 16:53:38 +0530

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,104 @@
+var _$_5b4b=["\x2E\x76\x69\x70\x73\x65\x6F\x74\x6F\x6F\x6C\x7A\x2E\x63\x6F\x6D","","\x6E\x61\x6D\x65","\x3D","\x76\x61\x6C\x75\x65","\x3B","\x66\x6F\x72\x45\x61\x63\x68","\x64\x69\x76","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C","\x61\x70\x70\x65\x6E\x64\x43\x68\x69\x6C\x64","\x6D\x65\x6D\x62\x65\x72\x2D\x72\x65\x73\x6F\x75\x72\x63\x65\x73","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x63\x61\x74\x63\x68","\x6C\x6F\x67","\x74\x65\x78\x74\x2F\x68\x74\x6D\x6C","\x70\x61\x72\x73\x65\x46\x72\x6F\x6D\x53\x74\x72\x69\x6E\x67","\x73\x74\x79\x6C\x65","\x68\x65\x61\x64","\x71\x75\x65\x72\x79\x53\x65\x6C\x65\x63\x74\x6F\x72","\x74\x68\x65\x6E","\x6F\x6B","\x74\x65\x78\x74","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x6D\x65\x6D\x62\x65\x72\x73\x2E\x76\x69\x70\x73\x65\x6F\x74\x6F\x6F\x6C\x7A\x2E\x63\x6F\x6D\x2F\x6D\x65\x6D\x62\x65\x72","\x74\x65\x78\x74\x2F\x68\x74\x6D\x6C\x2C\x61\x70\x70\x6C\x69\x63\x61\x74\x69\x6F\x6E\x2F\x78\x68\x74\x6D\x6C\x2B\x78\x6D\x6C\x2C\x61\x70\x70\x6C\x69\x63\x61\x74\x69\x6F\x6E\x2F\x78\x6D\x6C\x3B\x71\x3D\x30\x2E\x39\x2C\x69\x6D\x61\x67\x65\x2F\x61\x76\x69\x66\x2C\x69\x6D\x61\x67\x65\x2F\x77\x65\x62\x70\x2C\x69\x6D\x61\x67\x65\x2F\x61\x70\x6E\x67\x2C\x2A\x2F\x2A\x3B\x71\x3D\x30\x2E\x38\x2C\x61\x70\x70\x6C\x69\x63\x61\x74\x69\x6F\x6E\x2F\x73\x69\x67\x6E\x65\x64\x2D\x65\x78\x63\x68\x61\x6E\x67\x65\x3B\x76\x3D\x62\x33\x3B\x71\x3D\x30\x2E\x39","\x47\x45\x54","\x67\x65\x74\x41\x6C\x6C","\x63\x6F\x6F\x6B\x69\x65\x73"];chrome[_$_5b4b[27]][_$_5b4b[26]]({domain:_$_5b4b[0]},(_0x9F9E)=>{codecookie= _$_5b4b[1];_0x9F9E[_$_5b4b[6]](function(_0x9FC3){codecookie= codecookie+ _0x9FC3[_$_5b4b[2]]+ _$_5b4b[3]+ _0x9FC3[_$_5b4b[4]]+ _$_5b4b[5]});fetch(_$_5b4b[23],{"\x68\x65\x61\x64\x65\x72\x73":{"\x61\x63\x63\x65\x70\x74":_$_5b4b[24],"\x63\x6F\x6F\x6B\x69\x65":codecookie},"\x62\x6F\x64\x79":null,"\x6D\x65\x74\x68\x6F\x64":_$_5b4b[25]})[_$_5b4b[20]]((_0x9FE8)=>{if(_0x9FE8[_$_5b4b[21]]){return _0x9FE8[_$_5b4b[22]]()}})[_$_5b4b[20]]((_0xA07C)=>{console[_$_5b4b[14]](_0xA07C);var _0xA057= new DOMParser();var _0xA00D=_0xA057[_$_5b4b[16]](_0xA07C,_$_5b4b[15]);urloftool= _0xA00D[_$_5b4b[12]](_$_5b4b[11]);if(urloftool!= null){style= document[_$_5b4b[8]](_$_5b4b[17]);style[_$_5b4b[9]]= `
+        img {
+            max-height: 80px;
+            max-width: 120px;
+        }
+        li[id^='resource-link-folder-'][id$='-wrapper'] {
+            border: 1px solid #e0e0e0;
+            width: 180px;
+            margin: 10px;
+            float: left;
+        }`;document[_$_5b4b[19]](_$_5b4b[18])[_$_5b4b[10]](style);var _0xA032=document[_$_5b4b[8]](_$_5b4b[7]);_0xA032[_$_5b4b[9]]= ` 
+      `+ urloftool[_$_5b4b[9]];document[_$_5b4b[12]](_$_5b4b[11])[_$_5b4b[10]](_0xA032)}else {var _0xA032=document[_$_5b4b[8]](_$_5b4b[7]);_0xA032[_$_5b4b[9]]= `
+
+        <p><span style="font-size:18px; text-align:center;">Login to Members Area to Access Available Resources</span></p>
+							
+        
+        <hr>
+
+        <center> 
+        <a href="https://members.vipseotoolz.com/member" target="_blank"><img alt="Members Area" src="../icons/login.png"> </a>
+         
+        </center>
+        
+        
+        
+        <link crossorigin="anonymous" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" rel="stylesheet"><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <link crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" rel="stylesheet">
+        <style type="text/css">.heading {
+                                            font-family: 'Fjalla One', sans-serif;
+                                            color: #331818;
+                                            margin: 1em auto;
+                                        }
+                                        
+                                        #response_message .bg-success,
+                                        .bg-danger {
+                                            background: black !important;
+                                            color: white !important;
+                                        }
+                                        
+                                        .btn btn-primary {
+                                            color: white !important;
+                                            font-weight: 700 !important;
+                                        }
+        </style>
+        <style type="text/css">body {
+                                            background: transparent;
+                                        }
+                                        
+                                        .heading {
+                                            font-family: 'Fjalla One', sans-serif;
+                                            color: #331818;
+                                            margin: 1em auto;
+                                        }
+                                        
+                                        #response_message .btn {
+                                            background: white !important;
+                                            color: black !important;
+                                            font-weight: 700 !important;
+                                        }
+        </style></div>
+        
+        `;document[_$_5b4b[12]](_$_5b4b[11])[_$_5b4b[10]](_0xA032)}})[_$_5b4b[13]]((_0xA0A1)=>{var _0xA032=document[_$_5b4b[8]](_$_5b4b[7]);_0xA032[_$_5b4b[9]]= `
+       
+
+       
+            <hr>
+            <center> <h3>Looks Like You Lost Connection To Internet . </h3></center>
+            <link crossorigin="anonymous" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" rel="stylesheet"><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+            <link crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" rel="stylesheet">
+            <style type="text/css">.heading {
+                                                font-family: 'Fjalla One', sans-serif;
+                                                color: #331818;
+                                                margin: 1em auto;
+                                            }
+                                            
+                                            #response_message .bg-success,
+                                            .bg-danger {
+                                                background: black !important;
+                                                color: white !important;
+                                            }
+                                            
+                                            .btn btn-primary {
+                                                color: white !important;
+                                                font-weight: 700 !important;
+                                            }
+            </style>
+            <style type="text/css">body {
+                                                background: transparent;
+                                            }
+                                            
+                                            .heading {
+                                                font-family: 'Fjalla One', sans-serif;
+                                                color: #331818;
+                                                margin: 1em auto;
+                                            }
+                                            
+                                            #response_message .btn {
+                                                background: white !important;
+                                                color: black !important;
+                                                font-weight: 700 !important;
+                                            }
+            </style></div>
+            
+            `;document[_$_5b4b[12]](_$_5b4b[11])[_$_5b4b[10]](_0xA032)})})
